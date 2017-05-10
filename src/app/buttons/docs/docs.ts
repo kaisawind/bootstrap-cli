ffa273a6a7bdb977756f5e44896e8b1b3bb9056a
kaisawind <wind.kaisa@gmail.com>
2017-05-10 23:00:35 +0800

    删除无效文件

--- src/app/buttons/docs/docs.ts
+++ src/app/buttons/docs/docs.ts
@@ -1,11 +1,4 @@
 export const DOCS = {
-  usage: {
-    md: require('html-loader!markdown-loader!./usage.md')
-  },
-  basic: {
-    component: require('!!raw-loader?lang=typescript!./basic/basic'),
-    html: require('!!raw-loader?lang=markup!./basic/basic.html')
-  },
   types: {
     html: require('!!raw-loader?lang=markup!./types/types.html')
   },
